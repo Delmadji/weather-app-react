473faf6091346c1ef4a23e0f6a338c00fd4cf8cb
younes <ydelmadji@gmail.com>
2022-06-23 17:20:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
+const api = {
+  key: "4e4428be578b89f5630155a1e698c70a",
+  base: "https //api.openweathermap.org/data/2.5/",
+  apical: "api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}",
+  units: "imperial",
+};
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const [locGeo, setLocGeo] = useState({});
+  const [isPending, setIsPending] = useState(true);
+  let locLat = 36.7753606;
+  let locLon = 3.0601882;
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      setTimeout(() => {
+        fetch(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${query}&appid=${api.key}`
+        )
+          .then((res) => res.json())
+          .then((data) => setLocGeo(...data));
+
+        setQuery("");
+      }, 1000);
+    }
+  };
+  console.log(locGeo.lat, locGeo.lon);
+  useEffect(() => {
+    locLat = locGeo.lat;
+    locLon = locGeo.lon;
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${locLat}&lon=${locLon}&units${api.units}&appid=${api.key}`
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        setWeather(data);
+        setIsPending(false);
+      });
+    console.log("hiiiii");
+  }, [locGeo.lat, locGeo.lon]);
+  /*useEffect(() => {
+    
+  }, [locGeo.lat, locGeo.lon]);*/
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={
+        typeof weather.name != "undefined"
+          ? (weather.main.temp - 273.15).toFixed() > 20
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyDown={search}
+          />
+        </div>
+        {typeof weather.name != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {locGeo.name}, {locGeo.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {(weather.main.temp - 273.15).toFixed()}°c
+              </div>
+              <div className="weather">
+                {(weather.main.temp - 273.15).toFixed() > 20
+                  ? "Sunny"
+                  : "cloud"}
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
     </div>
   );
 }
